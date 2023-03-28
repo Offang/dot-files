8b4ed539f69d5bb048f41cf02c17836cb7591385
Offang <nicola.gn.paolucci@gmail.com>
2023-03-28 12:14:48 +0200

    Added bspwm

---
+++ bspwm/quit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+killall pa-applet
+killall nm-applet
+killall sxhkd
+killall dunst
+killall idle.sh
+killall eww
+killall media_info
+killall workspaces
+killall layout
+killall winevents.sh
+bspc quit
